7873222401a1ee655f0dbeb5fcb50a474ca64684
minee0505 <wodeyue97@gmail.com>
2025-06-25 21:04:37 +0900

    메서드 복습

--- src/chap1_2/method/MethodBasic.java
+++ src/chap1_2/method/MethodBasic.java
@@ -1,24 +1,36 @@
 package chap1_2.method;
 
 
+import java.util.Arrays;
+
 public class MethodBasic {
 
     // 자바는 함수라는 개념이 없음
-    // 메서드: 객체가 가진 기능
+    // 메서드: 객체가 가진 기능 ~~.~~();
     // 메서드는 정의할 수 있는 위치는 클래스 내부, 메서드 외부
     public static void main(String... args) {
         int result1 = add(20, 30);
         System.out.println("result1 = " + result1);
 
         int result2 = addAll(new int[]{1,2,3,4}); // 이 부분 gpt 질문
+        // int[]{1, 2, 3}같은 배열 리터럴은 선언과 동시에 초기화할 때만 사용 가능
+        // 파라미터로 쓸 때는 new int[]{...}처럼 명시적으로 생성
+        // int[]{...}만 쓰면 자바 문법상 에러
         System.out.println("result2 = " + result2);
 
         int result3 = addAll(10, 30, 50, 70);
 
         System.out.println("result3 = " + result3);
+
+        int[] ints = multiReturn(10, 20);
+        System.out.println(Arrays.toString(ints));
     }
 
-    // 2개의 정수를 받아서 더해서 반환
+    // 함수 만들 때 초보 방법 단계
+    // 1. 앞에 일단 void add(n1, n2) {return n1 + n2;}
+    // 2. 자바는 파라미터의 타입을 명시해줘야 함 void add(int n1,int n2) {return n1 + n2;}
+    // 3. 리턴값의 타입을 void 에다가 집어넣음 int add(int n1, int n2) {return n1 + n2;}
+
     static int add(int n1, int n2) {
         return n1 + n2;
     }
@@ -30,9 +42,10 @@ public class MethodBasic {
     }
     void bar() {
         System.out.println("hello!!");
-    }
+    } // 리턴 없으면 void
 
     // n개의 정수를 전달받아서 싹 더해서 리턴
+    // 대괄호 없애고 타입형 뒤에 ... 붙이면 얘가 알아서 배열로 묶어줌
     static int addAll(int... numbers){
         int total = 0;
         for (int n : numbers) {
@@ -41,16 +54,17 @@ public class MethodBasic {
         return total;
     }
 
-    // 배열 2개를 전달받을 때
+    // 배열 2개를 전달받을 때 ...하면 어디까지가 nums1 배열인지 모름, 이럴때는 호출하는 쪽에서 확실하게 끊어줘야 함
 
     static void ticky(int[] nums1, int[] nums2) {
         ticky(new int[]{1,2,3}, new int[]{4,5,6});
     }
 
     // 여러개를 리턴하고 싶을 때
-    int[] multiReturn(int n1, int n2) {
+    static int[] multiReturn(int n1, int n2) {
         return new int[] {n1 + n2, n1 - n2};
     }
+
 }
 
 
---
+++ src/chap1_2/method/MethodQuiz_Ref.java
@@ -0,0 +1,138 @@
+package chap1_2.method;
+
+import java.util.Arrays;
+
+public class MethodQuiz_Ref {
+
+    static String[] foods = {"떡볶이", "치킨", "파스타"};
+
+    public static void main(String[] args) {
+
+        printFoods();
+
+        push("라면");
+        push("김치찌개");
+        push("짬뽕", "닭강정");
+        printFoods();
+
+
+        int index = indexOf("파스타");
+        System.out.println("index = " + index);
+
+        int index2 = indexOf("라면땅");
+        System.out.println("index2 = " + index2);
+
+        pop();
+        remove("치킨");
+        remove(0);
+        printFoods();
+
+
+        boolean flag1 = include("파스타");
+        System.out.println("flag1 = " + flag1);
+
+        boolean flag2 = include("떡라면");
+        System.out.println("flag2 = " + flag2);
+
+        insert(1, "파인애플");
+
+        printFoods();
+
+        modify(2, "닭갈비");
+        printFoods();
+    } // end main
+
+
+    static void printFoods() {
+        System.out.println(Arrays.toString(foods));
+    }
+
+    static String[] copy(int capacity) {
+        String[] tempArray = new String[foods.length + capacity];
+
+        // 루프 회전수는 사이즈를 늘릴 때는 원본만큼
+        // 늘어난 부분엔 복사할 데이터가 없음, 즉, 원본 배열보다 더 돌면 foods[i]가 존재하지 않아서 ArrayIndexOutOfBoundsException 에러가 터짐
+        // 줄였을 때는 줄인 만큼 회전
+        int loopCount = (capacity >= 0) ? foods.length : tempArray.length;
+        for (int i = 0; i < loopCount; i++) {
+            tempArray[i] = foods[i];
+        }
+        return tempArray;
+    }
+
+    static void push(String... items) {
+        // 1. 사이즈가 1개 더 큰 배열을 생성
+        // 2. 원본 내용을 복사
+        String[] temp = copy(items.length);
+
+        // 3. 배열에 맨 끝에 데이터 추가
+        for (int i = 0; i < items.length; i++) {
+            temp[temp.length - (items.length - i)] = items[i];
+        }
+        // 4. 주소값 이전
+        foods = temp;
+//        temp = null; 지역변수여서 할 필요 없음
+    }
+
+    static int indexOf(String item) {
+        for (int i = 0; i < foods.length; i++) {
+            if (foods[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static String pop() {
+        if (foods.length == 0) {
+            System.out.println("제거할 대상이 없습니다.");
+            return null; // 깡 리턴은 보이드에서만 사용가능
+        }
+        String deleted = foods[foods.length - 1];
+
+        foods = copy(-1);
+        return deleted;
+    }
+
+    // 배열 저장값으로 찾아서 삭제
+    static void remove(String targetItem) {
+
+        if (!include(targetItem)) return;
+
+        for (int i = indexOf(targetItem); i < foods.length - 1; i++) {
+            foods[i] = foods[i + 1];
+        }
+        pop();
+    }
+
+    static boolean include(String item) {
+        return indexOf(item) != -1;
+    }
+
+    // 오버로딩 : 같은이름의 메서드를 계속만드는 행위
+    static void remove(int targetIndex) {
+        if (isOutOfBounds(targetIndex)) return;
+        remove(foods[targetIndex]);
+    }
+
+    static void insert(int index, String item) {
+        if (isOutOfBounds(index)) return;
+        String[] temp = copy(1);
+        for (int i = foods.length; i > index; i--) {
+            temp[i] = temp[i - 1];
+        }
+        temp[index] = item;
+        foods = temp;
+    }
+
+    static void modify(int index, String item) {
+        if (isOutOfBounds(index)) return;
+        foods[index] = item;
+    }
+
+    static boolean isOutOfBounds(int index) {
+        System.out.println("인덱스의 범위가 잘못되었습니다.");
+        return index < 0 || index >= foods.length;
+    }
+
+}
