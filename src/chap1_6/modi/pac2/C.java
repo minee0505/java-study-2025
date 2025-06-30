0d3183a6aae865426a085681adfddc3c60169d22
minee0505 <wodeyue97@gmail.com>
2025-06-30 21:31:25 +0900

    접근 제어자 및 캡슐화 연습: 클래스 A, B, C 및 Car 추가

---
+++ src/chap1_6/modi/pac2/C.java
@@ -0,0 +1,23 @@
+package chap1_6.modi.pac2;
+
+import chap1_6.modi.pac1.A;
+
+// A 클래스와 다른 패키지
+public class C {
+
+    C() {
+        A a = new A();
+//        new A(10);
+//        new A(1.2);
+
+        a.f1 = 100;
+//        a.f2 = 200;
+//        a.f3 = 300;
+
+        a.m1();
+//        a.m2();
+//        a.m3();
+
+//        new B();
+    }
+}
