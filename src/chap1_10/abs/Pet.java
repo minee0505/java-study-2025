4597a3f5d64eb594c284d4ba024e5437202e92a2
minee0505 <wodeyue97@gmail.com>
2025-07-03 10:40:26 +0900

    추상 클래스와 다형성 예제 추가: Pet 클래스 설계 및 Dog, Cat, Tiger 구현, Main 클래스에서 다형성 활용 예제 구현.

---
+++ src/chap1_10/abs/Pet.java
@@ -0,0 +1,12 @@
+package chap1_10.abs;
+
+// 추상적
+// 추상 클래스: 실체화된 객체가 아닌 다형성을 위함 껍데기 클래스 (설계도가 아님, 설계도를 만들기 위한 규격서)
+// 오버라이딩을 강제할 수 있음
+public abstract class Pet {
+
+    // 추상 메서드: 오버라이딩을 위한 껍데기 틀
+    public abstract void feed();
+    public abstract void sleep();
+
+}
